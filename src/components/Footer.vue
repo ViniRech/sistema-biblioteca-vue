cdd355f38e95941f70ab2669bdb8427749f014a3
ViniRech <viniciusrech2007@gmail.com>
2024-08-11 17:54:51 -0300

    footer creation

---
+++ src/components/Footer.vue
@@ -0,0 +1,106 @@
+<script setup></script>
+<template>
+    <footer class="footer">
+        <section class="footer-description">
+            <h1><router-link to="/" exact>NOME BIBLIOTECA</router-link></h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+        </section>
+
+        <section class="footer-links">
+            <div>
+                <h4>A Biblioteca</h4>
+                <ul>
+                    <li>Sobre nós</li>
+                    <li>Nossa equipe</li>
+                    <li>Participação</li>
+                    <li>Colaboradores</li>
+                </ul>
+            </div>
+            <div>
+                <h4>A ONG</h4>
+                <ul>
+                    <li>Sobre a ONG</li>
+                    <li>Projetos</li>
+                    <li>Site oficial</li>
+                </ul>
+            </div>
+            <div>
+                <h4>Os Livros</h4>
+                <ul>
+                    <li>Histórico</li>
+                    <li>Doação</li>
+                    <li>Locais físicos</li>
+                    <li>Acervo</li>
+                </ul>
+            </div>
+        </section>
+    </footer>
+</template>
+
+<style scoped>
+    .footer {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        background-color: var(--vt-c-dark-blue);
+        color: var(--vt-c-white);
+        padding: 3rem;
+    }
+
+    .footer-description {
+        display: grid;
+        row-gap: 1rem;
+        width: 70%;
+        text-align: justify;
+    }
+
+    .footer-description h1 a {
+        color: var(--vt-c-white);
+    }
+
+    .footer-links {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        text-align: left;
+    }
+
+    .footer-links div h4 {
+        padding-bottom: .8rem;
+    }
+
+    .footer-links div ul li {
+        padding: .2rem 0
+    }
+
+    @media (max-width: 775px) {
+        .footer {
+            display: grid;
+            grid-template-columns: 1fr;
+            justify-content: center;
+            row-gap: 2rem;
+        }
+
+        .footer-description {
+            width: 100%;
+        }
+
+        .footer-description h1 {
+            text-align: center;
+        }
+    }
+
+    @media (max-width: 450px) {
+        .footer {
+            padding: 2rem;
+        }
+
+        .footer-links {
+            grid-template-columns: 1fr 1fr;
+            padding-left: .5rem;
+            row-gap: 2rem;
+            column-gap: 1rem;
+        }
+    }
+</style>
